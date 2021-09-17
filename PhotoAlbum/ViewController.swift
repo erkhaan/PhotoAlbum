6f843bd4adcf72cc01bf3e7b58f7e713f242a935
notzie <notykt@gmail.com>
2021-09-17 21:28:12 +0900

    First commit

---
+++ PhotoAlbum/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PhotoAlbum
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
+
